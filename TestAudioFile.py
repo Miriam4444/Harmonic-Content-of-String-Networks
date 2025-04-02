1df10821affb4a84d9f1aba611ef1aef12e136ab
Miriam4444 <146225355+Miriam4444@users.noreply.github.com>
2025-04-02 01:31:34 -0400

    Update TestAudioFile.py

--- TestAudioFile.py
+++ TestAudioFile.py
@@ -4,17 +4,34 @@ from AudioFile import AudioFile
 class TestAudioFile:
 
     if __name__ == "__main__":
+
+        # initialize an array to house the desired samples
         nameArray = AudioFilesArray()
-        nameList = nameArray.getSpecificType()
+
+        # choose the sample type you want to analyze
+        nameList = nameArray.getSpecificType() 
 
         # print length of nameList:
-        print("amount of files in nameList: ", nameList)
+        print("amount of files in nameList: ", len(nameList))
 
-        #iterate through each element of nameList or do one at a time
-        sample = AudioFile(nameList[0])
+        # iterate through each element of nameList 
+        for i in range(len(nameList)):
+            sample = AudioFile(nameList[i])
 
-        # graph the harmonic spectrum
+            # graph the harmonic spectrum
+            sample.graph_magspec_withWindowedPeaks(80)
+
+            # graph the correlation between experimentally determined harmonic data to theoretical model
+            sample.graphRatioArray(80)
+
+        # Choose a specific sample
+        sample = AudioFile(nameList[0]) # If you would like to analyze a different sample, change the index appropriately
+
+        # Graph the harmonic spectrum
         sample.graph_magspec_withWindowedPeaks(80)
 
-        #graph the correlation between experimentally determined harmonic data to theoretical model
+        # Graph the correlation between experimentally determined harmonic data to theoretical model
         sample.graphRatioArray(80)
+
+        # Calculate the aggregate error for the samples in the array
+        print(AudioFile.printAggregateError())
